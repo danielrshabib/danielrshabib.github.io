349c0c619ef9fcc3ffd67f101a0517f79339d487
Daniel Roy Sadek Habib <danielrsh@aol.com>
2024-08-25 08:24:54 -0400

    Add files via upload

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,1 +1,52 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Boston, MA, USA | American Public Health Association Annual Meeting | 2022 | Segregation, coronavirus racial bias, and relationships with COVID-19 distress",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Atlanta, GA, USA | American Public Health Association Annual Meeting | 2023 | Novel substance use and support themes on Reddit: Lessons for researchers and practitioners",
+    33.7489924,
+    -84.3902644
+  ],
+  [
+    "Nashville, TN, USA | American Society and Joint Section of Stereotactic and Functional Neurosurgery Biennial Meeting | 2024 | Mood and cognitive effects of deep brain stimulation for essential tremor",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Nashville, TN, USA | American Academy of Otolaryngology-Head and Neck Surgery Foundation Annual Meeting | 2023 | Flap to floor: Free flap comparative outcomes outside the ICU",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Chicago, IL, USA | Combined Otolaryngology Spring Meetings | 2024 | Augmented reality in facial plastic & reconstructive surgery: A systematic review",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Boston, MA, USA | American Public Health Association Annual Meeting | 2022 | Trends in the dehumanization of people who use drugs in American news articles from 1986 to 2020",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Baltimore, MD, USA | American Society of Colon and Rectal Surgeons Annual Scientific Meeting | 2024 | NCCN guideline adherence in rectal cancer and outcomes in low-volume vs. high-volume centers",
+    39.2908816,
+    -76.610759
+  ],
+  [
+    "New Orleans, LA, USA | Society of Asian Academic Surgeons Annual Meeting | 2024 | Impact of neoadjuvant immunotherapy on postoperative complications after oncologic surgery",
+    29.9759983,
+    -90.0782127
+  ],
+  [
+    "San Francisco, CA, USA | Surgical Outcomes Club Annual Scientific Session | 2024 | Association of neoadjuvant immunotherapy with postoperative major morbidity after oncologic surgery",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Boston, MA, USA | Combined Otolaryngology Spring Meetings | 2023 | How often is cancer present in oral cavity re-resections after initial positive margins?",
+    42.3554334,
+    -71.060511
+  ]
+];
