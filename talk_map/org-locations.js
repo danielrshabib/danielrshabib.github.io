312b2f4eb0aa05217684a5fc7a6176ec762d4523
Daniel Roy Sadek Habib <danielrsh2000@gmail.com>
2025-08-15 16:09:41 -0500

    Add files via upload

---
+++ talk_map/org-locations.js
@@ -0,0 +1,132 @@
+var addressPoints = [
+  [
+    "Boston, MA, USA | American Public Health Association Annual Meeting | 2022 | Segregation, coronavirus racial bias, and relationships with COVID-19 distress",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Chicago, IL, USA | American College of Surgeons Clinical Congress | 2025 | Mortality from digestive cancers among Asian American subgroups in United States",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Association between hearing aid and cochlear implant internet searches with media campaigns and federal announcements",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Atlanta, GA, USA | American Public Health Association Annual Meeting | 2023 | Novel substance use and support themes on Reddit: Lessons for researchers and practitioners",
+    33.7544657,
+    -84.3898151
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Self-paced sinusitis educational module for medical students: Improving otolaryngology knowledge for all future specialties",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Association of preoperative depression and clinical outcomes after head and neck free flap reconstruction",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Nashville, TN, USA | American Society for Stereotactic and Functional Neurosurgery Biennial Meeting | 2024 | Mood and cognitive effects of deep brain stimulation for essential tremor",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "New Orleans, LA, USA | American Head and Neck Society Annual Meeting at COSM | 2025 | Lymph node yield is not associated with survival in total laryngectomy patients",
+    29.9559145,
+    -90.0730259
+  ],
+  [
+    "Boston, MA, USA | American Association of Neurological Surgeons Annual Meeting | 2025 | Neuropsychological effects of GPi deep brain stimulation in medication resistant dystonia",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Risk factors for increased postoperative pain in head and neck cancer free flap reconstruction patients",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Madrid, Spain | International Facial Nerve Symposium | 2025 | The unseen burden of non-flaccid facial paralysis: Patient-reported psychosocial impact and treatment experience",
+    40.4167047,
+    -3.7035825
+  ],
+  [
+    "Nashville, TN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2023 | Flap to floor: Free flap comparative outcomes outside the ICU",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Sonoma, CA, USA | Southwestern Surgical Congress Annual Meeting | 2025 | Patient-reported non-routine events for same-day discharge after mastectomy",
+    38.5110803,
+    -122.8473388
+  ],
+  [
+    "Chicago, IL, USA | American Academy of Facial Plastic and Reconstructive Surgery Annual Meeting at COSM | 2024 | Augmented reality in facial plastic & reconstructive surgery: A systematic review",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Palm Springs, CA, USA | American Cleft Palate-Craniofacial Association Annual Meeting | 2025 | Speech outcomes and surgical care in Pierre Robin sequence",
+    33.8246269,
+    -116.540303
+  ],
+  [
+    "New Orleans, LA, USA | American Head and Neck Society Annual Meeting at COSM | 2025 | Outcomes following neoadjuvant immunotherapy for oral cavity cancer: A propensity score matched analysis of the National Cancer Database",
+    29.9559145,
+    -90.0730259
+  ],
+  [
+    "Boston, MA, USA | American Public Health Association Annual Meeting | 2022 | Trends in the dehumanization of people who use drugs in American news articles from 1986 to 2020",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Orlando, FL, USA | Triological Combined Sections Meeting | 2025 | Risk factors and rates for hypocalcemia after pediatric thyroidectomy: A systematic review and meta-analysis",
+    28.5421109,
+    -81.3790304
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Preoperative tracheostomy is associated with recurrence in total laryngectomy patients",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Baltimore, MD, USA | American Society of Colon and Rectal Surgeons Annual Scientific Meeting | 2024 | NCCN guideline adherence in rectal cancer and outcomes in low-volume vs. high-volume centers",
+    39.2908816,
+    -76.610759
+  ],
+  [
+    "New Orleans, LA, USA | Society of Asian Academic Surgeons Annual Meeting | 2024 | Impact of neoadjuvant immunotherapy on postoperative complications after oncologic surgery",
+    29.9559145,
+    -90.0730259
+  ],
+  [
+    "San Francisco, CA, USA | Surgical Outcomes Club Annual Scientific Session | 2024 | Association of neoadjuvant immunotherapy with postoperative major morbidity after oncologic surgery",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Indianapolis, IN, USA | American Academy of Otolaryngology-Head and Neck Surgery Annual Meeting | 2025 | Trends in cochlear implant complications and clinical outcomes from 2016 to 2024",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "Madrid, Spain | International Facial Nerve Symposium | 2025 | Patient-reported barriers to diagnosis and treatment in non-flaccid facial paralysis",
+    40.4167047,
+    -3.7035825
+  ],
+  [
+    "Boston, MA, USA | Triological Society Annual Meeting at COSM | 2023 | How often is cancer present in oral cavity re-resections after initial positive margins?",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "New Orleans, LA, USA | American Head and Neck Society Annual Meeting at COSM | 2025 | Adjuvant chemotherapy does not improve overall survival in positive surgical margin oral cavity cancer patients without extranodal extension",
+    29.9559145,
+    -90.0730259
+  ]
+];
